b97b8ec47828ef1bda270599232a6f2b13a1ee66
Russell Frame <Russell.Frame@filmtrack.net>
2020-02-05 09:55:36 -0800

    Add Bootstrap and home page

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {CoreRoutingModule} from './core.routing.module';
+import {RouterModule} from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CoreRoutingModule,
+  ],
+  exports: [
+    RouterModule
+  ],
+})
+export class CoreModule { }
---
+++ src/app/core/core.routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('../home/home.module').then(m => m.HomeModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {
+    scrollPositionRestoration: 'enabled',
+    anchorScrolling: 'enabled'
+  })],
+  exports: [RouterModule]
+})
+export class CoreRoutingModule { }
